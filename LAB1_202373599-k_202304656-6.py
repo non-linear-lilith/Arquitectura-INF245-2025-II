5bd7453e57ccc0428a32953078aa42bb66807db3
non-linear-lilith <khuidobro@usm.cl>
2025-08-27 17:47:54 -0400

    agregadas las funcinoes hacia decimal

--- LAB1_202373599-k_202304656-6.py
+++ LAB1_202373599-k_202304656-6.py
@@ -6,5 +6,13 @@ def main():
     decimal = ModuloB.binario_a_decimal(binario)
     print(f"El número binario {binario} en decimal es {decimal}")
 
+    octal = "100"
+    decimal_octal = ModuloB.oct_to_decimal(octal)
+    print(f"El número octal {octal} en decimal es {decimal_octal}")
+    
+    hexadecimal = "1A3F"
+    decimal_hex = ModuloB.hex_to_decimal(hexadecimal)
+    print(f"El número hexadecimal {hexadecimal} en decimal es {decimal_hex}")
+
 if __name__ == "__main__":
     main()
--- ModuloB.py
+++ ModuloB.py
@@ -4,7 +4,23 @@ def binario_a_decimal(cadena_binaria: str) -> int:
     longitud: int = len(cadena_binaria)
     for i in range(longitud):
         if(cadena_binaria[longitud-i-1]=='1'):
-
             resultado =resultado + 2**(i)
- 
+    return resultado
+
+def oct_to_decimal(cadena_octal: str) -> int:
+    resultado = 0
+    longitud: int = len(cadena_octal)
+    for i in range(longitud):
+        resultado += int(cadena_octal[longitud-i-1]) * 8**i
+    return resultado
+
+def hex_to_decimal(cadena_hex: str) -> int:
+    resultado = 0
+    longitud: int = len(cadena_hex)
+    for i in range(longitud):
+        digito = cadena_hex[longitud-i-1]
+        if '0' <= digito <= '9':
+            resultado += (ord(digito) - ord('0')) * 16**i
+        elif 'A' <= digito <= 'F':
+            resultado += (ord(digito) - ord('A') + 10) * 16**i
     return resultado
